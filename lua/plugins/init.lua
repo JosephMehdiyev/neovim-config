3775798374eac9ae8438d62ad54f4cd98607f7eb
Yusif Mehdiyev <yusifmehdiyev55@gmail.com>
2024-09-28 17:50:34 +0400

    config update!

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -15,6 +15,7 @@ require('lazy').setup({
         init = function()
             -- VimTeX configuration goes here, e.g.
             vim.g.vimtex_view_method = 'zathura'
+            vim.g.vimtex_delim_enabled = 0
         end,
     },
 
--- lua/set.lua
+++ lua/set.lua
@@ -2,5 +2,6 @@ vim.cmd 'TransparentEnable'
 vim.cmd.colorscheme 'onedark'
 vim.cmd.set 'shiftwidth=4 smarttab'
 vim.cmd.set 'expandtab'
+vim.g.vimtex_syntax_enabled = 0
 vim.opt.termguicolors = true
 require('bufferline').setup {}
